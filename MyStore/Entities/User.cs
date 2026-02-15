063dbeb77d53012e0d607fd5160e1b46f1e0aa9a
Jorge <jorge.medi.08@gmail.com>
2026-02-14 18:05:31 -0600

    feat: Cracion de clases para las entidades de la bd

---
+++ MyStore/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStore.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public int Password { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+
+        public ICollection<Order> Orders  { get; set; }
+    }
+}
