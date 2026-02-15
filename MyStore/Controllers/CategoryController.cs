b949707824d4265a18ccf9f2d76e85e373b3797a
Jorge <jorge.medi.08@gmail.com>
2026-02-15 16:18:47 -0600

    feat: Vista Rezor para listar las categorias

---
+++ MyStore/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStore.Context;
+
+namespace MyStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly AppDbContext context;
+
+        public CategoryController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+
+            var categories = context.Category.ToList();
+            return View(categories);
+        }
+    }
+}
