675bf062b850d6d29289438b47346d9e0901312f
Jorge <jorge.medi.08@gmail.com>
2026-02-16 14:19:31 -0600

    feature: Clase generic agregue los metodos basicos CRUD

--- MyStore/Repositories/GenericRepository.cs
+++ MyStore/Repositories/GenericRepository.cs
@@ -18,6 +18,30 @@ namespace MyStore.Repositories
                 .AsNoTracking()
                 .ToListAsync();
         }
+
+        public async Task AddAsync(TEntity entity) 
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<TEntity?>GetByIdAsync(int entityid)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(entityid);
+        }
+
+        public async Task EditAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
     }
 
 }
